87f429a6693657518f1a36b538aeec1462b59b6a
WalmirJr20 <walmirovelha2@gmail.com>
2024-10-23 15:58:47 -0300

    criarMenu
    Criação da cena do menu

---
+++ My project/Assets/MenuPrincipalManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+}
